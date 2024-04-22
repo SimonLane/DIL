3acf1f3f1791020fb74438cb3bc88f2821a0e865
Simon Lane <simon.lane@soton.ac.uk>
2024-04-22 22:52:36 +0100

    Update z_stack.py

--- z_stack.py
+++ z_stack.py
@@ -13,33 +13,69 @@ Created on Mon Apr 22 13:06:57 2024
 # =============================================================================
 
 nZ          = 10    #Number of slices
-z_space     = 10    #slice separation (micrometers)
+sZ          = 10    #slice separation (micrometers)
 exp         = 100   #camera exposure time (ms)
 
-
+# =============================================================================
+GO_COM = 'COM3'
+DIL_COM = 'COM5'
+FILTER_COM = 'COM6'
 # =============================================================================
 #  IMPORTS
 # =============================================================================
 
 from pylablib.devices import DCAM
-import serial
+import serial, time
 
 
 # =============================================================================
-# Connections
+# SCRIPT
 # =============================================================================
-#CAMERA
+#CAMERA connection
 print('n cameras', DCAM.get_cameras_number())
 cam = DCAM.DCAMCamera()
 
-#STAGE
-GO = serial.Serial(port=port, baudrate=115200, timeout=0.2)
+#STAGE serial connection
+GO = serial.Serial(port=GO_COM, baudrate=115200, timeout=0.2)
 
-#CONTROLLER
+#CONTROLLER serial connection
+DIL = serial.Serial(port=DIL_COM, baudrate=115200, timeout=0.2)
 
-DIL = serial.Serial(port=port, baudrate=115200, timeout=0.2)
+# SETUP - CAMERA
+# allocate buffer
+# set mode, binning, exposure
+# set up trigger
 
+
+# SETUP - STAGE
+# move to start position. Perform z-stack centered around current position
+start_move = nZ*sZ/-2
+GO.write(b"MRZ%s" %(start_move))
+# set trigger in
+GO.write(b"TIZ%s" %(sZ))
+# set trigger out
+GO.write(b"TOZ5s" %(sZ))
+# poll for stage movement
+while(stage_movement()):
+    time.sleep(0.1)
+    
+# Activate Z-scan on DIL controller
+DIL.write(b"/Stack.%s.%s.%s\r" %(nZ, sZ, exp))
+
+# poll camera for images
+# start timer
 # =============================================================================
 # close connections
 # =============================================================================
 cam.close()
+DIL.close()
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def stage_movement():
+    pass
+    #get stage status
+    #if moving return True
+    #if not moving return false
